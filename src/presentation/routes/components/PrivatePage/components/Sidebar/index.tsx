b7e12c7f009a11ec9f6a986080fd30e789cd055a
Aron <aronviniciusnascimento@gmail.com>
2023-12-14 14:29:35 -0300

    fix: activatedMenu

--- src/presentation/routes/components/PrivatePage/components/Sidebar/index.tsx
+++ src/presentation/routes/components/PrivatePage/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import {
   BurgerMenu,
   BurgerMenuContainer,
@@ -8,19 +8,57 @@ import {
   MenuItem,
   Sidebar,
 } from "./styles";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../../../../utils/useAuth";
 import { ROUTES_PATHS } from "../../../../../constants/routePaths";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 export const SideBar = () => {
-  const [isMobile, setIsMobile] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-  const [activeItem, setActiveItem] = useState(null);
-
+  const location = useLocation();
   const navigate = useNavigate();
   const { logout, user } = useAuth();
+
+  const [isMobile, setIsMobile] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState('');
+
+  const userPermissions = useMemo(() => {
+    return user?.Permissions ?? [];
+  }, [user]);
+  
+  const menuItems = useMemo(() => {
+    return ROUTES_PATHS.filter(
+      (route) => route.isPrivate && userPermissions.includes(route.title)
+    ).map((route) => ({
+      name: route.title,
+      path: route.path,
+      onClick: () => navigate(route.path),
+      icon: route.icon,
+    }));
+  }, [userPermissions, navigate]);
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
+  const logoutItem = {
+    name: "Sair",
+    onClick: handleLogout,
+  };
+
+  useEffect(() => {
+    const pathname = location.pathname;
+    const activeRoute = ROUTES_PATHS.find((route) =>
+      pathname.startsWith(route.path)
+    );
+
+    if (activeRoute && userPermissions.includes(activeRoute.title)) {
+      setActiveItem(activeRoute.title);
+    }
+  }, [location.pathname, userPermissions]);
+
 
   const checkIfMobile = () => {
     setIsMobile(window.innerWidth <= 767);
@@ -38,107 +76,92 @@ export const SideBar = () => {
 
   const handleMenuItemClick = (item) => {
     setActiveItem(item.name);
+    navigate(item.path);
     item.onClick();
+    if (isMobile) {
+      setIsOpen(false);
+    }
   };
 
-  const handleLogout = () => {
-    logout();
-    navigate("/");
-  };
 
-  const userPermissions = user && user.Permissions ? user.Permissions : [];
-
-  const menuItems = ROUTES_PATHS.filter(
-    (route) => route.isPrivate && userPermissions.includes(route.title) // Filter based on permissions
-  ).map((route) => ({
-    name: route.title,
-    onClick: () => navigate(route.path),
-    icon: route.icon,
-  }));
-
-  // Adicionar item "Sair" para o logout
-  const logoutItem = {
-    name: "Sair",
-    onClick: handleLogout,
-  };
 
   return (
     <>
-      <Header>
-        {isMobile && (
-          <>
-            <img
-              src="/logo.png"
-              alt="Logo"
-              style={{ maxWidth: "100px", height: "auto" }}
-            />{" "}
-            <BurgerMenu onClick={toggleMenu}>
-              <BurgerMenuContainer>
-                <MenuIcon />
-                <MenuIcon />
-                <MenuIcon />
-              </BurgerMenuContainer>
-            </BurgerMenu>
-          </>
-        )}
-      </Header>
-      {isMobile && isOpen && (
-        <Menu>
-          {menuItems.map((item) => (
-           <MenuItem
-                onClick={() => {
-                  if (userPermissions.includes(item.name)) {
-                    handleMenuItemClick(item);
-                    
-                    setIsOpen(false); // Fechar o menu após clicar em um item
-                  }
-                }}
-                key={item.name}
-                className={`${activeItem === item.name ? "activated" : ""} ${
-                  !userPermissions.includes(item.name) ? "disabled" : ""
-                }`}
-                disabled={!userPermissions.includes(item.name)}
-              >
-                <FontAwesomeIcon icon={item.icon as IconProp} />{" "}
-                {/* Ícone antes do texto */}
-                {item.name}
-              </MenuItem>
-          ))}
-          <MenuItem onClick={() => handleMenuItemClick(logoutItem)}>
-            {logoutItem.name}
-          </MenuItem>
-        </Menu>
-      )}
-      {!isMobile && (
-        <Sidebar>
+    <Header>
+      {isMobile && (
+        <>
           <img
             src="/logo.png"
             alt="Logo"
-            style={{ maxWidth: "100%", height: "auto" }}
+            style={{ maxWidth: "100px", height: "auto" }}
           />{" "}
-          <Menu>
-            {menuItems.map((item) => (
-              <MenuItem
-                onClick={() => {
-                  if (userPermissions.includes(item.name)) {
-                    handleMenuItemClick(item);
-                  }
-                }}
-                key={item.name}
-                className={`${activeItem === item.name ? "activated" : ""} ${
-                  !userPermissions.includes(item.name) ? "disabled" : ""
-                }`}
-                disabled={!userPermissions.includes(item.name)}
-              >
-                <FontAwesomeIcon icon={item.icon as IconProp} />{" "}
-                {/* Ícone antes do texto */}
-                {item.name}
-              </MenuItem>
-            ))}
-            <MenuItem onClick={logoutItem.onClick}>{logoutItem.name}</MenuItem>
-          </Menu>
-        </Sidebar>
+          <BurgerMenu onClick={toggleMenu}>
+            <BurgerMenuContainer>
+              <MenuIcon />
+              <MenuIcon />
+              <MenuIcon />
+            </BurgerMenuContainer>
+          </BurgerMenu>
+        </>
       )}
-    </>
-  );
+    </Header>
+    {isMobile && isOpen && (
+      <Menu>
+        {menuItems.map((item) => (
+         <MenuItem
+              onClick={() => {
+                if (userPermissions.includes(item.name)) {
+                  handleMenuItemClick(item);
+                  
+                  setIsOpen(false); // Fechar o menu após clicar em um item
+                }
+              }}
+              key={item.name}
+              className={`${activeItem === item.name ? "activated" : ""} ${
+                !userPermissions.includes(item.name) ? "disabled" : ""
+              }`}
+              disabled={!userPermissions.includes(item.name)}
+            >
+              <FontAwesomeIcon icon={item.icon as IconProp} />{" "}
+              {/* Ícone antes do texto */}
+              {item.name}
+            </MenuItem>
+        ))}
+        <MenuItem onClick={() => handleMenuItemClick(logoutItem)}>
+          {logoutItem.name}
+        </MenuItem>
+      </Menu>
+    )}
+    {!isMobile && (
+      <Sidebar>
+        <img
+          src="/logo.png"
+          alt="Logo"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />{" "}
+        <Menu>
+          {menuItems.map((item) => (
+            <MenuItem
+              onClick={() => {
+                if (userPermissions.includes(item.name)) {
+                  handleMenuItemClick(item);
+                }
+              }}
+              key={item.name}
+              className={`${activeItem === item.name ? "activated" : ""} ${
+                !userPermissions.includes(item.name) ? "disabled" : ""
+              }`}
+              disabled={!userPermissions.includes(item.name)}
+            >
+              <FontAwesomeIcon icon={item.icon as IconProp} />{" "}
+              {/* Ícone antes do texto */}
+              {item.name}
+            </MenuItem>
+          ))}
+          <MenuItem onClick={logoutItem.onClick}>{logoutItem.name}</MenuItem>
+        </Menu>
+      </Sidebar>
+    )}
+  </>
+);
 };
