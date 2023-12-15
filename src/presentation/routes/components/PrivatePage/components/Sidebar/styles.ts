28af273961d698f96d860386b82e50909e23192f
Aron <aronviniciusnascimento@gmail.com>
2023-12-15 16:34:28 -0300

    fix: header mobile

--- src/presentation/routes/components/PrivatePage/components/Sidebar/styles.ts
+++ src/presentation/routes/components/PrivatePage/components/Sidebar/styles.ts
@@ -44,9 +44,9 @@ export const Menu = styled.ul`
     height: 100%;
     box-shadow: 2px 0 4px rgba(0, 0, 0, 0.1); /* Sombra desejada */
     background-color: #fff;
-    top: 11.4%;
+    top: 9.5%;
     position: absolute;
-    z-index: 1;
+    z-index: 999;
     margin: 0;
   }
 `;
