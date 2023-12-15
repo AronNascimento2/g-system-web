91c0ffdc7a433a6d704258cf7a091fcab02da28a
Aron <aronviniciusnascimento@gmail.com>
2023-12-14 23:52:10 -0300

    fix: responsiveLyout

--- src/presentation/pages/Schedules/components/HeaderButtonsMobile/index.tsx
+++ src/presentation/pages/Schedules/components/HeaderButtonsMobile/index.tsx
@@ -2,16 +2,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ModalOptionsMobile } from "../ModalOptionsMobile";
 import { Container } from "./styles";
 import { faArrowsRotate, faFilter } from "@fortawesome/free-solid-svg-icons";
-import { DynamicButton } from "../../../../components/DynamicButton";
 
-export const HeaderButtonsMobile = ({update}) => {
+export const HeaderButtonsMobile = ({ update }) => {
   return (
     <Container>
       <button className="open-options-button" onClick={update}>
         <FontAwesomeIcon className="collapsed-icon" icon={faArrowsRotate} />
       </button>
       <input className="input-search" type="text" placeholder="Buscar" />
-      <DynamicButton className='button' icon={faFilter} text={'Filtrar'}/>
+      <button className="open-options-button" onClick={update}>
+        <FontAwesomeIcon className="collapsed-icon" icon={faFilter} />
+      </button>{" "}
       <ModalOptionsMobile />
     </Container>
   );
