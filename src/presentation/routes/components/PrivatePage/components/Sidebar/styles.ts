39d51174d6c2fbaa4253f74c31eee1852fca4cd6
Aron <aronviniciusnascimento@gmail.com>
2023-12-16 23:09:01 -0300

    fix: padding on menu

--- src/presentation/routes/components/PrivatePage/components/Sidebar/styles.ts
+++ src/presentation/routes/components/PrivatePage/components/Sidebar/styles.ts
@@ -82,6 +82,7 @@ export const MenuItem = styled.li<SidebarProps>`
 
   @media (max-width: 1080px) {
     color: black;
+    padding:  0.5rem 1rem;
   }
   &.activated {
     background-color: #0098da;
