9abe2c97e0cf3b68fc36bb7f16f18419caa1274f
Aron <aronviniciusnascimento@gmail.com>
2023-12-29 08:11:41 -0300

    feat: lock screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import { AppRouter } from "./presentation/routes/AppRouter";
 import { AuthProvider } from "./presentation/contexts/AuthProvider";
 import "./App.css";
-import LandscapeLock from "./land";
 
 export const App: React.FC = () => {
   return (
     <AuthProvider>
-      <LandscapeLock/>
       <AppRouter />
     </AuthProvider>
   );
